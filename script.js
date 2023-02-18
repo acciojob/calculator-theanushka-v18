347df2ed2619e58d3b4b099897ae596ee23165a4
Anushka Verma <anushkaverma779@gmail.com>
2023-02-18 11:34:18 +0530

    done

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+let ans1 ="";
+let num ="";
+function add(value){
+    console.log(value);
+    num=num+value;
+    document.getElementById('input').value=num;
+
+}
+
+
+function ans(){
+    ans1=eval(num);
+    document.getElementById('input').value=ans1;
+    ans1="";
+    num="";
+}
+
+function clear(){
+    num="";
+    document.getElementById('input').value="";
+}
